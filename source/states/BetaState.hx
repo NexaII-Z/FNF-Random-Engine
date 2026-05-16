16022591f7e219f4a0636ad3657568503ae04151
NexaII <jawadwajdisaeed@gmail.com>
2026-05-16 10:19:49 +0300

    Update BetaState.hx

--- source/states/BetaState.hx
+++ source/states/BetaState.hx
@@ -1,13 +1,21 @@
 package states;
 
-class OutdatedState extends MusicBeatState
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.text.FlxText;
+import flixel.tweens.FlxTween;
+import flixel.util.FlxColor;
+
+class BetaState extends MusicBeatState
 {
 	public static var leftState:Bool = false;
 
 	var warnText:FlxText;
+
 	override function create()
 	{
 		super.create();
+		leftState = false;
 
 		var bg:FlxSprite = new FlxSprite().makeGraphic(FlxG.width, FlxG.height, FlxColor.BLACK);
 		add(bg);
@@ -16,15 +24,11 @@ class OutdatedState extends MusicBeatState
 
 		if (controls.mobileC)
 		{
-			guh = "Also, This Engine Is in Beta   \n
-			Which Means You Will Find Some Bugs While Playing \n
-        you can reort them in the Random Engine Github, Or Gamebanana Website
-          Press A To Continue.";
-		} else {
-			guh = "Also, This Engine Is in Beta   \n
-			Which Means You Will Find Some Bugs While Playing \n
-        you can reort them in the Random Engine Github, Or Gamebanana Website
-          Press Enter To Continue.";
+			guh = "Also, This Engine Is in Beta\nWhich Means You Will Find Some Bugs While Playing.\nYou can report them on the Random Engine Github or Gamebanana.\nPress A To Continue.";
+		}
+		else
+		{
+			guh = "Also, This Engine Is in Beta\nWhich Means You Will Find Some Bugs While Playing.\nYou can report them on the Random Engine Github or Gamebanana.\nPress Enter To Continue.";
 		}
 
 		warnText = new FlxText(0, 0, FlxG.width, guh, 32);
@@ -39,18 +43,16 @@ class OutdatedState extends MusicBeatState
 
 	override function update(elapsed:Float)
 	{
-		if(!leftState) {
+		if (!leftState)
+		{
 			if (controls.ACCEPT)
 			{
 				leftState = true;
-			}
-
-			if(leftState)
-			{
-				FlxG.sound.play(Paths.sound('cancelMenu'));
+				FlxG.sound.play(Paths.sound('confirmMenu'));
 				FlxTween.tween(warnText, {alpha: 0}, 1, {
-					onComplete: function (twn:FlxTween) {
-						MusicBeatState.switchState(new MainMenuState());
+					onComplete: function(twn:FlxTween)
+					{
+						MusicBeatState.switchState(new TitleState());
 					}
 				});
 			}
