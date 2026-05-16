5f6ffd69589786ff81d1f6279750f2a273d80806
NexaII <jawadwajdisaeed@gmail.com>
2026-05-16 10:20:20 +0300

    Update FlashingState.hx

--- source/states/FlashingState.hx
+++ source/states/FlashingState.hx
@@ -1,55 +1,46 @@
 package states;
 
-import flixel.FlxSubState;
-
+import flixel.FlxG;
+import flixel.FlxSprite;
 import flixel.effects.FlxFlicker;
-import lime.app.Application;
+import flixel.text.FlxText;
+import flixel.tweens.FlxTween;
+import flixel.util.FlxColor;
+import flixel.util.FlxTimer;
 import flixel.addons.transition.FlxTransitionableState;
+import lime.app.Application;
 
 class FlashingState extends MusicBeatState
 {
 	public static var leftState:Bool = false;
 
-	#if mobile
-	var warnTextMobile:FlxText;
-	#else
 	var warnText:FlxText;
-	#end
-	
+
 	override function create()
 	{
 		super.create();
+		leftState = false;
 
 		var bg:FlxSprite = new FlxSprite().makeGraphic(FlxG.width, FlxG.height, FlxColor.BLACK);
 		add(bg);
 
-		#if mobile
-		var guhMobile:String = "Hey, watch out!\n
-		This Engine contains some flashing lights!\n
-		Press A to disable them now or go to Options Menu.\n
-		Press B to ignore this message.\n
-		You've been warned!";
-		#else
-		var guh:String = "Hey, watch out!\n
-		This Engine contains some flashing lights!\n
-		Press ENTER to disable them now or go to Options Menu.\n
-		Press ESCAPE to ignore this message.\n
-		You've been warned!";
-		#end
-		
-		controls.isInSubstate = false; // qhar I hate it
+		var guh:String;
 
-		#if mobile
-		warnTextMobile = new FlxText(0, 0, FlxG.width, guhMobile, 32);
-		warnTextMobile.setFormat("VCR OSD Mono", 32, FlxColor.WHITE, CENTER);
-		warnTextMobile.screenCenter(Y);
-		add(warnTextMobile);
-		#else
+		if (controls.mobileC)
+		{
+			guh = "Hey, watch out!\nThis Engine contains some flashing lights!\nPress A to disable them now or go to Options Menu.\nPress B to ignore this message.\nYou've been warned!";
+		}
+		else
+		{
+			guh = "Hey, watch out!\nThis Engine contains some flashing lights!\nPress ENTER to disable them now or go to Options Menu.\nPress ESCAPE to ignore this message.\nYou've been warned!";
+		}
+
+		controls.isInSubstate = false;
+
 		warnText = new FlxText(0, 0, FlxG.width, guh, 32);
 		warnText.setFormat("VCR OSD Mono", 32, FlxColor.WHITE, CENTER);
 		warnText.screenCenter(Y);
 		add(warnText);
-		#end
 
 		#if mobile
 		addTouchPad("NONE", "A_B");
@@ -58,46 +49,38 @@ class FlashingState extends MusicBeatState
 
 	override function update(elapsed:Float)
 	{
-		if(!leftState) {
+		if (!leftState)
+		{
 			var back:Bool = controls.BACK;
-			if (controls.ACCEPT || back) {
+			if (controls.ACCEPT || back)
+			{
 				leftState = true;
 				FlxTransitionableState.skipNextTransIn = true;
 				FlxTransitionableState.skipNextTransOut = true;
-				if(!back) {
+
+				if (!back)
+				{
 					ClientPrefs.data.flashing = false;
 					ClientPrefs.saveSettings();
 					FlxG.sound.play(Paths.sound('confirmMenu'));
-					
-					#if mobile
-					FlxFlicker.flicker(warnTextMobile, 1, 0.1, false, true, function(flk:FlxFlicker) {
-						new FlxTimer().start(0.5, function (tmr:FlxTimer) {
+
+					FlxFlicker.flicker(warnText, 1, 0.1, false, true, function(flk:FlxFlicker)
+					{
+						new FlxTimer().start(0.5, function(tmr:FlxTimer)
+						{
 							MusicBeatState.switchState(new TitleState());
 						});
 					});
-					#else
-					FlxFlicker.flicker(warnText, 1, 0.1, false, true, function(flk:FlxFlicker) {
-						new FlxTimer().start(0.5, function (tmr:FlxTimer) {
-							MusicBeatState.switchState(new TitleState());
-						});
-					});
-					#end
-				} else {
+				}
+				else
+				{
 					FlxG.sound.play(Paths.sound('cancelMenu'));
-					
-					#if mobile
-					FlxTween.tween(warnTextMobile, {alpha: 0}, 1, {
-						onComplete: function (twn:FlxTween) {
-							MusicBeatState.switchState(new BetaState());
-						}
-					});
-					#else
 					FlxTween.tween(warnText, {alpha: 0}, 1, {
-						onComplete: function (twn:FlxTween) {
+						onComplete: function(twn:FlxTween)
+						{
 							MusicBeatState.switchState(new BetaState());
 						}
 					});
-					#end
 				}
 			}
 		}
