45a92fa35dedbc19b5c81dae67e98d1fdf6cfe8b
NexaII <jawadwajdisaeed@gmail.com>
2026-05-17 22:15:12 +0300

    Update VisualsUISubState.hx

--- source/options/VisualsUISubState.hx
+++ source/options/VisualsUISubState.hx
@@ -7,52 +7,21 @@ import objects.Alphabet;
 class VisualsUISubState extends BaseOptionsMenu
 {
 	public static var pauseMusics:Array<String> = ['None', 'Breakfast', 'Tea Time'];
-	var noteOptionID:Int = -1;
-	var notes:FlxTypedGroup<StrumNote>;
-	var notesTween:Array<FlxTween> = [];
-	var noteY:Float = 90;
+
 	public function new()
 	{
 		title = 'Visuals and UI';
 		rpcTitle = 'Visuals & UI Settings Menu'; //for Discord Rich Presence
 
-		// for note skins
-		notes = new FlxTypedGroup<StrumNote>();
-		for (i in 0...Note.colArray.length)
-		{
-			var note:StrumNote = new StrumNote(370 + (560 / Note.colArray.length) * i, -200, i, 0);
-			note.centerOffsets();
-			note.centerOrigin();
-			note.playAnim('static');
-			notes.add(note);
-		}
+		// Note Skins and Note Splashes moved to dedicated NoteSkinState
 
-		// options
-
-		var noteSkins:Array<String> = Mods.mergeAllTextsNamed('images/noteSkins/list.txt');
-		if(noteSkins.length > 0)
-		{
-			if(!noteSkins.contains(ClientPrefs.data.noteSkin))
-				ClientPrefs.data.noteSkin = ClientPrefs.defaultData.noteSkin; //Reset to default if saved noteskin couldnt be found
-
-			noteSkins.insert(0, ClientPrefs.defaultData.noteSkin); //Default skin always comes first
-			var option:Option = new Option('Note Skins:',
-				"Select your prefered Note skin.",
-				'noteSkin',
-				'string',
-				noteSkins);
-			addOption(option);
-			option.onChange = onChangeNoteSkin;
-			noteOptionID = optionsArray.length - 1;
-		}
-		
 		var noteSplashes:Array<String> = Mods.mergeAllTextsNamed('images/noteSplashes/list.txt');
 		if(noteSplashes.length > 0)
 		{
 			if(!noteSplashes.contains(ClientPrefs.data.splashSkin))
-				ClientPrefs.data.splashSkin = ClientPrefs.defaultData.splashSkin; //Reset to default if saved splashskin couldnt be found
+				ClientPrefs.data.splashSkin = ClientPrefs.defaultData.splashSkin;
 
-			noteSplashes.insert(0, ClientPrefs.defaultData.splashSkin); //Default skin always comes first
+			noteSplashes.insert(0, ClientPrefs.defaultData.splashSkin);
 			var option:Option = new Option('Note Splashes:',
 				"Select your prefered Note Splash variation or turn it off.",
 				'splashSkin',
@@ -161,24 +130,6 @@ class VisualsUISubState extends BaseOptionsMenu
 		addOption(option);
 
 		super();
-		add(notes);
-	}
-
-	override function changeSelection(change:Int = 0)
-	{
-		super.changeSelection(change);
-		
-		if(noteOptionID < 0) return;
-
-		for (i in 0...Note.colArray.length)
-		{
-			var note:StrumNote = notes.members[i];
-			if(notesTween[i] != null) notesTween[i].cancel();
-			if(curSelected == noteOptionID)
-				notesTween[i] = FlxTween.tween(note, {y: noteY}, Math.abs(note.y / (200 + noteY)) / 3, {ease: FlxEase.quadInOut});
-			else
-				notesTween[i] = FlxTween.tween(note, {y: -200}, Math.abs(note.y / (200 + noteY)) / 3, {ease: FlxEase.quadInOut});
-		}
 	}
 
 	var changedMusic:Bool = false;
@@ -190,26 +141,6 @@ class VisualsUISubState extends BaseOptionsMenu
 			FlxG.sound.playMusic(Paths.music(Paths.formatToSongPath(ClientPrefs.data.pauseMusic)));
 
 		changedMusic = true;
-	}
-
-	function onChangeNoteSkin()
-	{
-		notes.forEachAlive(function(note:StrumNote) {
-			changeNoteSkin(note);
-			note.centerOffsets();
-			note.centerOrigin();
-		});
-	}
-
-	function changeNoteSkin(note:StrumNote)
-	{
-		var skin:String = Note.defaultNoteSkin;
-		var customSkin:String = skin + Note.getNoteSkinPostfix();
-		if(Paths.fileExists('images/$customSkin.png', IMAGE)) skin = customSkin;
-
-		note.texture = skin; //Load texture and anims
-		note.reloadNote();
-		note.playAnim('static');
 	}
 
 	override function destroy()
